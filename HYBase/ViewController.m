422dad50e00663333a8f722ad0e3fde1ccc0db08
5P_JackXu <151611438@qq.com>
2020-01-08 14:56:34 +0800

    Initial Commit

---
+++ HYBase/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HYBase
+//
+//  Created by 5P on 2020/1/8.
+//  Copyright © 2020 JXRice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ HYBaseTests/HYBaseTests.m
@@ -0,0 +1,37 @@
+//
+//  HYBaseTests.m
+//  HYBaseTests
+//
+//  Created by 5P on 2020/1/8.
+//  Copyright © 2020 JXRice. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HYBaseTests : XCTestCase
+
+@end
+
+@implementation HYBaseTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
